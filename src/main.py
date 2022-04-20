2a2f16b1d9aacb0fba045ad10f11d299031db27a
AndreasPB <andreas.p.brodersen@gmail.com>
2022-04-20 10:52:00 +0200

    Add httpexception

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI, HTTPException
 
 
 app = FastAPI()
 
+
+messages = {"1": [{"id": "10", "message": "Hi", "access": "*"}]}
+
+
 @app.get("/")
 async def index():
     return "(◡ ‿ ◡ ✿)"
+
+@app.get("/provider/{id}/from/{last_message_id}/limit/{limit}/token/{token}")
+async def idk(id, last_message_id, limit, token):
+    try:
+        return json.dumps(messages[id])
+    except Exception as e:
+        return HTTPException(status_code=204)
